a865d829d3e4b26f8b837949d30f0d59e11a2445
Yaqut88 <yaqutquliyeva88@gmail.com>
2024-03-01 11:53:03 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+class CV {
+    constructor(name, age, telephone, email) {
+        this.name = name;
+        this.age = age;
+        this.telephone = telephone;
+        this.email = email;
+        this.education = [];
+        this.workExperience = [];
+        this.abilities = [];
+    }
+
+    addEducation(highSchollName, degree, startYear, endYear) {
+        this.education.push({ highSchollName, degree, startYear, endYear })
+    }
+    addWorkExperience(companyName, position, startYear, endYear) {
+        this.workExperience.push({ companyName, position, startYear, endYear })
+    }
+    addAbility(ability1, ability2) {
+        this.abilities.push({ ability1, ability2 })
+    }
+
+    cvAdd() {
+        console.log(`Name: ${this.name} Age: ${this.age}  Telephone: ${this.telephone}  Email: ${this.email} `)
+        console.log("Education:");
+        this.education.forEach(e => { console.log(`${e.highSchollName}, ${e.degree}, ${e.startYear}, ${e.endYear}`) });
+        console.log("Work Experience:");
+        this.workExperience.forEach(i => { console.log(`${i.companyName}, ${i.position}, ${i.startYear}, ${i.endYear}`) })
+        console.log("Abilities:")
+        this.abilities.forEach(b => { console.log(`${b}`) })
+    }
+
+}
+
+
+let myCV = new CV("Mehri", 35, "0123333333", "mehri@gmail.com");
+myCV.addEducation("Baku State University", "Master", "2010", "2016")
+myCV.addWorkExperience("Company Name", "Position", "2020", "2024")
+myCV.addAbility("")
+
+myCV.cvAdd();
+
